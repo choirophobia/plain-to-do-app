5d649661290542376ffabb7106badbdcb5cc8ec1
fikriccc <fikriahmadi96@gmail.com>
2025-11-20 17:09:03 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,56 @@
+todos = []
+
+
+def add_todo():
+    task = input('Enter a new task: ')
+    todos.append(task)
+    print('task has been added!')
+    
+
+
+
+def delete_todo():
+
+    view_todo()
+    pickANum = int(input('Enter number to be deleted : '))
+    if 1 <= pickANum <= len(todos):
+        removed = todos.pop(pickANum - 1)
+        print(f'removed : {removed} \n')
+    else : 
+        print('invalid number!!! Please retry!')
+
+
+
+def view_todo():
+    print("your todos are: ")
+    if len(todos) == 0:
+        print('no todos yet! Please add one')
+    else:
+        for i, task in enumerate(todos, start=1):
+            print(f'{i}. {task}')
+    
+
+
+
+def main():
+    while True:
+        print("this is todo app")
+        print('please choose ; ')
+        print('1. view todo')
+        print('2. add todo')
+        print('3. delete todo')
+
+        num = input("choose an option: ")
+
+        if num == '1':
+            view_todo()
+        elif num == '2':
+            add_todo()
+        elif num == '3':
+            delete_todo()
+        else:
+            print('invalid option')
+
+
+
+main()
